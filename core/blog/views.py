d9bf7449dd3addff3d599ef1c3a1ccc0eedb605d
amirkhodaei1 <amirkhodaei8415@gmail.com>
2026-08-05 09:16:57 +0330

    commit

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from .models import Post
+
 # Create your views here.
 def indexView(request):
-    context={"name":"Amirhossein"}
-    return render(request,"blog/index.html",context)
+    context = {"name": "Amirhossein"}
+    return render(request, "blog/index.html", context)
+
+
+class IndexView(TemplateView):
+    template_name = "blog/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name']="ali"   
+        context['posts']=Post.objects.all()
+        return context
