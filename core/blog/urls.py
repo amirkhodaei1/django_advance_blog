bdbea73b93ca4308e8ecfe857234aeb28cb01a4e
amirkhodaei1 <amirkhodaei8415@gmail.com>
2026-08-05 11:49:45 +0330

    commit

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -4,6 +4,8 @@ from django.views.generic.base import RedirectView
 app_name='blog'
 urlpatterns=[
     # path('fbv_index',views.indexView,name="fbv_index"),
-    path('cbv_index',views.IndexView.as_view(),name='cbv_index'),
+    path('cbv_index',views.IndexView.as_view(),name='cbv-index'),
+    path('post/<int:pk>/',views.PostList.as_view(),name="post-list"),
+
     path('go_to_maktabkhooneh/<int:pk>/',views.RedirectToMaktab.as_view(),name="go_to_maktabkhooneh")
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView
+
 from .models import Post
 
 # 1. Function-Based View (برای مسیر fbv_index)
+"""
 def indexView(request):
     context = {"name": "Amirhossein"}
     return render(request, "blog/index.html", context)
+"""
 
 
 # 2. Class-Based View (برای نمایش صفحه اصلی)
@@ -19,11 +23,37 @@ class IndexView(TemplateView):
         return context
 
 
+# 3. function-Based View (برای ریدایرکت)
+
+""" 
+def redirect_to_maktab_view(request, pk):
+    # بررسی وجود پست (در صورت عدم وجود، خطای 404 می‌دهد)
+    post = get_object_or_404(Post, pk=pk)
+    print(post)
+    
+    # ریدایرکت موقت (HTTP 302)
+    return redirect('https://maktabkhooneh.org')
+
+    # اگر ریدایرکت دائمی (HTTP 301) مد نظر شما باشد:
+    # return redirect('https://maktabkhooneh.org', permanent=True)
+"""
+
+
 # 3. Class-Based View (برای ریدایرکت)
 class RedirectToMaktab(RedirectView):
-    url = 'https://maktabkhooneh.org'
+    url = "https://maktabkhooneh.org"
 
     def get_redirect_url(self, *args, **kwargs):
-        post = get_object_or_404(Post, pk=kwargs['pk'])
+        post = get_object_or_404(Post, pk=kwargs["pk"])
         print(post)
         return super().get_redirect_url(*args, **kwargs)
+
+
+class PostList(ListView):
+    # model=Post
+    # queryset=Post.objects.all
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        posts = Post.objects.filter(status=True)
+        return posts
