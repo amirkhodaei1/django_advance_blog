abe9b9378be3cb7dc7614a4473c33f5d3ecb8c08
amirkhodaei1 <amirkhodaei8415@gmail.com>
2026-08-05 10:53:16 +0330

    commit

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path 
 from . import views
-<<<<<<< HEAD
 from django.views.generic.base import RedirectView
 app_name='blog'
 urlpatterns=[
@@ -8,11 +7,3 @@ urlpatterns=[
     path('cbv_index',views.IndexView.as_view(),name='cbv_index'),
     path('go_to_maktabkhooneh/<int:pk>/',views.RedirectToMaktab.as_view(),name="go_to_maktabkhooneh")
 ]
-=======
-from django.views.generic import TemplateView
-urlpatterns=[
-    path('fbv_index',views.indexView,name="fbv_index"),
-    path('cbv_index',views.IndexView.as_view(),name="cbv_index")
-
-]
->>>>>>> origin/main
