046a6f8691a8a641ec271dc79c3cc5a084539d32
amirkhodaei1 <amirkhodaei8415@gmail.com>
2026-08-07 07:38:31 +0330

    commit

--- core/blog/models.py
+++ core/blog/models.py
@@ -22,3 +22,4 @@ class Category(models.Model):
     name =models.CharField(max_length=250)
     def __str__(self):
         return self.name
+    
--- core/blog/urls.py
+++ core/blog/urls.py
@@ -7,6 +7,9 @@ urlpatterns=[
     path('post/',views.PostListView.as_view(),name="post-list"),
     path('post/<int:pk>/',views.PostDetailView.as_view(),name="post-detail"),
     path('post/create/',views.PostCreateView.as_view(),name="post-create"),
+    path('post/<int:pk>/edit',views.PostEditView.as_view(),name="post-edit"),
+    path('post/<int:pk>/delete',views.PostDeleteView.as_view(),name="post-delete"),
+    
 
     path('go_to_maktabkhooneh/<int:pk>/',views.RedirectToMaktab.as_view(),name="go_to_maktabkhooneh")
 ]
