78b364658a0b27ec978c94f1669e8923cbae542f
amirkhodaei1 <amirkhodaei8415@gmail.com>
2026-08-26 10:09:06 +0330

    edit

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse,JsonResponse
+import time
+def send_email(request):
+    send_email.delay()
+    return HttpResponse("<h1>Done Sending</h1>")
+def test(request):
+    response=request.get("https://e9d281d2-ddac-4cb5-be91-418470426aac.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
